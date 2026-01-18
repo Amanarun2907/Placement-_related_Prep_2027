c10d4d742b53b7c8160d6747ade0a47a5e149db3
Amanarun2907 <aman29.10.2004@gmail.com>
2026-01-18 18:37:53 +0530

    done
    DONE

--- java Chapter 16 Recursion part1 (Basics)/First_and_last_occ.java
+++ java Chapter 16 Recursion part1 (Basics)/First_and_last_occ.java
@@ -23,11 +23,41 @@ public class First_and_last_occ {
         first_last(s, p, idx+1);
 
     }
+    
 
+    public static boolean sorted (int arr[] , int idx){
+        // recurrsion
+        // array is given , now true is array is sorted and false is not sorted
+        // if array is ascending then true otherwise false .....  
+        // base case 
+        if (idx == arr.length-1){
+            return true ; 
+        }
+        // kaam 
+        if(arr[idx]>arr[idx+1]){
+            return false ;
+        }
+        return sorted (arr , idx+1);
+
+    }
 
     public static void main (String [] args){
         String s = "abaacdaefaah" ; 
         char  p = 'a';
         first_last (s , p , 0);
+
+
+
+    int arr1 [] = {1,2,3,4,5,6,7,8,9,10};
+    int arr2 [] = {1,2,3,4,6,5,7,9,10};
+    int arr3[] = {34,35,36,37,39,100};
+    System.out.println(sorted(arr1 , 0));
+    System.out.println(sorted(arr2 , 0));
+    System.out.println(sorted(arr3 , 0));
+
+
+
+
+    
     }
 }
